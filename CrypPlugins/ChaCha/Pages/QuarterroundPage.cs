1d0617fdd41a17aa080627bb94cdae19ceccb963
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-07-28 18:11:11 +0200

    Added CS file for QR Page

---
+++ CrypPlugins/ChaCha/Pages/QuarterroundPage.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Cryptool.Plugins.ChaCha
+{
+    public partial class ChaChaPresentation : UserControl, INotifyPropertyChanged
+    {
+        private Page QuarterroundPage()
+        {
+            Page p = new Page(UIQuarterroundPage);
+            PageAction showInput = new PageAction()
+            {
+                exec = () =>
+                {
+                    Add(QRInA, new Run(ConstantsLittleEndian.Replace(" ", "").Substring(0, 8)));
+                    Add(QRInB, new Run(KeyLittleEndian.Replace(" ", "").Substring(0, 8)));
+                    Add(QRInC, new Run(KeyLittleEndian.Replace(" ", "").Substring(InputKey.Length == 16 ? 0 : 32, 8)));
+                    Add(QRInD, new Run(InitialCounterLittleEndian.Replace(" ", "").Substring(0, 8)));
+                },
+                undo = Undo
+            };
+            p.AddAction(showInput);
+            return p;
+        }
+    }
+}
