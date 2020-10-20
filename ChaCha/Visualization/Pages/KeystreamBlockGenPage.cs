251f3d1ddf65fa35c8aaf57e4e0ae9baf5e9bcb1
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-10-20 03:01:36 +0200

    fix typo

--- ChaCha/Visualization/Pages/KeystreamBlockGenPage.cs
+++ ChaCha/Visualization/Pages/KeystreamBlockGenPage.cs
@@ -20,7 +20,7 @@ namespace Cryptool.Plugins.ChaCha
         private const string ACTIONLABEL_ROUND_START = "ROUND";
         private const string ACTIONLABEL_QR_END = "QUARTERROUND_END";
         public const string ACTIONLABEL_ADDITION_START = "ADDITION_START";
-        private const string ACTIONLABEL_ADDITION_END = "ADDITION_EMD";
+        private const string ACTIONLABEL_ADDITION_END = "ADDITION_END";
         public const string ACTIONLABEL_LITTLE_ENDIAN_START = "LITTLE_ENDIAN_START";
         private const string ACTIONLABEL_LITTLE_ENDIAN_END = "LITTLE_ENDIAN_END";
 
