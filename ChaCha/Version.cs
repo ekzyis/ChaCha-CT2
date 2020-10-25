4031115870edd40eca405c82018e0390cd6080f0
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-10-25 22:20:57 +0100

    Remove initial counter from version

--- ChaCha/Version.cs
+++ ChaCha/Version.cs
@@ -2,19 +2,17 @@
 {
     public class Version
     {
-        public static readonly Version IETF = new Version("IETF", 32, 96, 1);
-        public static readonly Version DJB = new Version("DJB", 64, 64, 0);
+        public static readonly Version IETF = new Version("IETF", 32, 96);
+        public static readonly Version DJB = new Version("DJB", 64, 64);
         public string Name { get; }
         public uint CounterBits { get; }
         public uint IVBits { get; }
-        public uint InitialCounter { get; }
 
-        private Version(string name, uint counterBits, uint ivBits, uint initialCounter)
+        private Version(string name, uint counterBits, uint ivBits)
         {
             Name = name;
             CounterBits = counterBits;
             IVBits = ivBits;
-            InitialCounter = initialCounter;
         }
     }
 }
