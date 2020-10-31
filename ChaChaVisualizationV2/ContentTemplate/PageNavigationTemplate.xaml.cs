02301cd48a0da8e1f38d643af348eadf784ac94a
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-10-31 15:23:28 +0100

    Created PageNavigationTemplate

---
+++ ChaChaVisualizationV2/ContentTemplate/PageNavigationTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.ContentTemplate
+{
+    /// <summary>
+    /// Interaction logic for PageNavigationTemplate.xaml
+    /// </summary>
+    public partial class PageNavigationTemplate : ResourceDictionary
+    {
+        public PageNavigationTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
