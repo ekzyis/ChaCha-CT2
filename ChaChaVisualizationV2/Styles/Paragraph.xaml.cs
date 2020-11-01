4de5facc843b98d45451b454818ec63b0efb4587
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-01 18:57:54 +0100

    Styling refactor

---
+++ ChaChaVisualizationV2/Styles/Paragraph.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Styles
+{
+    /// <summary>
+    /// Interaction logic for Paragraph.xaml
+    /// </summary>
+    public partial class Paragraph : ResourceDictionary
+    {
+        public Paragraph()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ChaChaVisualizationV2/Styles/TableCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Styles
+{
+    /// <summary>
+    /// Interaction logic for TableCell.xaml
+    /// </summary>
+    public partial class TableCell : ResourceDictionary
+    {
+        public TableCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
