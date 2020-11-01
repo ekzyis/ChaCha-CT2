558f745ebff573019ea1a1775b305fd781a65b1f
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-01 21:11:08 +0100

    Create simple hex converter

--- ChaChaVisualizationV2/Helper/Converter/ToHex.cs
+++ ChaChaVisualizationV2/Helper/Converter/ToHex.cs
@@ -1,41 +1,17 @@
-﻿using Cryptool.Plugins.ChaCha;
-using System;
+﻿using System;
 using System.Globalization;
-using System.Numerics;
 using System.Windows.Data;
 
 namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
 {
-    internal class ToHex : IMultiValueConverter
+    internal class ToHex : IValueConverter
     {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var value = values[0];
-            if (value is byte[] bytes)
-            {
-                return Formatter.HexString(bytes);
-            }
-            else if (value is BigInteger bigInteger)
-            {
-                var version = ((ChaChaSettings)values[1]).Version;
-                if (version.CounterBits == 64)
-                {
-                    return Formatter.HexString((ulong)bigInteger);
-                }
-                else
-                {
-                    return Formatter.HexString((uint)bigInteger);
-                }
-            }
-            else
-            {
-                // For some reason, the bindings in the ToHex Converter in Diffusion view are updated unset values when moving away from Diffusion page.
-                // throw new ArgumentException("value was neither byte[] nor BigInteger");
-                return null;
-            }
+            return Formatter.HexString((byte[])value);
         }
 
-        public object[] ConvertBack(object values, Type[] targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
---
+++ ChaChaVisualizationV2/Helper/Converter/ToHexWithVersion.cs
@@ -0,0 +1,43 @@
+﻿using Cryptool.Plugins.ChaCha;
+using System;
+using System.Globalization;
+using System.Numerics;
+using System.Windows.Data;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
+{
+    internal class ToHexWithVersion : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var value = values[0];
+            if (value is byte[] bytes)
+            {
+                return Formatter.HexString(bytes);
+            }
+            else if (value is BigInteger bigInteger)
+            {
+                var version = ((ChaChaSettings)values[1]).Version;
+                if (version.CounterBits == 64)
+                {
+                    return Formatter.HexString((ulong)bigInteger);
+                }
+                else
+                {
+                    return Formatter.HexString((uint)bigInteger);
+                }
+            }
+            else
+            {
+                // For some reason, the bindings in the ToHex Converter in Diffusion view are updated unset values when moving away from Diffusion page.
+                // throw new ArgumentException("value was neither byte[] nor BigInteger");
+                return null;
+            }
+        }
+
+        public object[] ConvertBack(object values, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
