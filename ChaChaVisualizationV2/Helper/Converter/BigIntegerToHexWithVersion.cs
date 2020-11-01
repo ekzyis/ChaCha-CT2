183af1eecfd6f7e79c41bacae58a132e37019613
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-01 23:29:13 +0100

    Remove debug assertion

--- ChaChaVisualizationV2/Helper/Converter/BigIntegerToHexWithVersion.cs
+++ ChaChaVisualizationV2/Helper/Converter/BigIntegerToHexWithVersion.cs
@@ -29,7 +29,6 @@ namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
         public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
         {
             string hex = (string)value;
-            System.Diagnostics.Debug.Assert(hex.Length == 16 || hex.Length == 24, $"value must be of length 16 or 24. Actual: {hex.Length}");
             // 64-bit are 8 bytes thus if the hex string has 16 characters, it has 8 bytes and is the counter of version DJB.
             ChaCha.Version v = hex.Length == 16 ? ChaCha.Version.DJB : ChaCha.Version.IETF;
             return new object[] { Formatter.BigInteger((string)value), v };
