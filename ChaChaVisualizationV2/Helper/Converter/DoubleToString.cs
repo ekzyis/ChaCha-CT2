02ed828f4c227cd28503130108a551e3f40ea706
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-02 15:10:03 +0100

    Add DoubleToString Converter

---
+++ ChaChaVisualizationV2/Helper/Converter/DoubleToString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
+{
+    internal class DoubleToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double d = (double)value;
+            return string.Format("{0:0.00}", d);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
