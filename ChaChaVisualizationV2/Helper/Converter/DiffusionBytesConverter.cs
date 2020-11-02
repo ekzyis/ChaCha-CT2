f91f0ed948b0524c283ff73182353e6ecf2a95a4
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-02 01:00:57 +0100

    Set validation rules and converters from code-behind

---
+++ ChaChaVisualizationV2/Helper/Converter/DiffusionBytesConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
+{
+    internal class DiffusionBytesConverter : IValueConverter
+    {
+        /// <summary>
+        /// Specifies how long the hex string should be. If hex string is too small, zero-left-padding is applied.
+        /// </summary>
+        private int Padding { get; set; }
+
+        public DiffusionBytesConverter(int padding) : base()
+        {
+            Padding = padding;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string hex = Formatter.HexString((byte[])value);
+            return hex.PadLeft(Padding).Replace(" ", "0");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string inputText = (string)value;
+            // Left-pad hex string with zero such that is has an even amount of characters.
+            if (inputText.Length % 2 == 1)
+            {
+                inputText = $"0{inputText}";
+            }
+            return Formatter.Bytes(inputText);
+        }
+    }
+}
---
+++ ChaChaVisualizationV2/Helper/Validation/DiffusionInputValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Validation
+{
+    internal class DiffusionInputValidationRule : ValidationRule
+    {
+        private int MaxKeyBytesLength { get; set; }
+        private int MaxHexKeyStringLength { get; set; }
+
+        public DiffusionInputValidationRule(int maxKeyBytesLength) : base()
+        {
+            MaxKeyBytesLength = maxKeyBytesLength;
+            MaxHexKeyStringLength = maxKeyBytesLength / 2;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string inputText = (string)value;
+            if (!System.Text.RegularExpressions.Regex.IsMatch(inputText, @"\A\b[0-9a-fA-F]+\b\Z"))
+            {
+                return new ValidationResult(false, $"Input is not a valid hex string.");
+            }
+
+            if (inputText.Length > MaxHexKeyStringLength)
+            {
+                return new ValidationResult(false, $"Hex input exceeds maximum key size of {MaxKeyBytesLength} bytes.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
