e424e5f958170b4acae5367b982ef99de2f9c6b9
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-03 20:39:25 +0100

    Add diffusion matrix entries

---
+++ ChaChaVisualizationV2/Helper/Converter/BooleanAndVisibility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
+{
+    internal class BooleanAndVisibility : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.All(v => (bool)v == true) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
