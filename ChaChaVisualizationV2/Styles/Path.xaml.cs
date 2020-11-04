3470470819b6e644fd77d64d186448df1fb60dd0
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-04 17:05:13 +0100

    (WIP) Add QR visualization XAML

---
+++ ChaChaVisualizationV2/Styles/Path.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Styles
+{
+    /// <summary>
+    /// Interaction logic for Path.xaml
+    /// </summary>
+    public partial class Path : ResourceDictionary
+    {
+        public Path()
+        {
+            InitializeComponent();
+        }
+    }
+}
