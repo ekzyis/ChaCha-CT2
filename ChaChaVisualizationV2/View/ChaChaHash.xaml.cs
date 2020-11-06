ce0d25f7fb77af86d72a5dc5dc0b0b4a0b681283
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-06 06:53:38 +0100

    Add action slider listener

--- ChaChaVisualizationV2/View/ChaChaHash.xaml.cs
+++ ChaChaVisualizationV2/View/ChaChaHash.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cryptool.Plugins.ChaChaVisualizationV2.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Cryptool.Plugins.ChaChaVisualizationV2.View
 {
@@ -23,6 +12,23 @@ namespace Cryptool.Plugins.ChaChaVisualizationV2.View
         public ChaChaHash()
         {
             InitializeComponent();
+            this.DataContextChanged += OnDataContextChanged;
+        }
+
+        private ChaChaHashViewModel ViewModel { get; set; }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ChaChaHashViewModel ViewModel = (ChaChaHashViewModel)e.NewValue;
+            if (ViewModel != null)
+            {
+                this.ViewModel = ViewModel;
+
+                // Add value changed event handler to action slider
+                Root.ApplyTemplate();
+                Slider actionSlider = (Slider)Root.Template.FindName("ActionSlider", Root);
+                actionSlider.ValueChanged += ViewModel.ActionSliderValueChange;
+            }
         }
     }
 }
