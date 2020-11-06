89669b655d4fa6190fb1f74f1bd6793fb1dc7f45
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-06 08:45:07 +0100

    Implement QRStep#GetEnumerator

--- ChaChaVisualizationV2/Model/QRStep.cs
+++ ChaChaVisualizationV2/Model/QRStep.cs
@@ -1,4 +1,6 @@
-﻿namespace Cryptool.Plugins.ChaChaVisualizationV2.Model
+﻿using System.Collections;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Model
 {
     /// <summary>
     /// Class for each step during one quarterround.
@@ -8,5 +10,16 @@
         public QRValue Add { get; set; }
         public QRValue XOR { get; set; }
         public QRValue Shift { get; set; }
+
+        /// <summary>
+        /// Get enumerator for all quarterround values of this step.
+        /// </summary>
+        /// <remarks>
+        /// Needed for `foreach`.
+        /// </remarks>
+        public IEnumerator GetEnumerator()
+        {
+            return new QRValue[] { Add, XOR, Shift }.GetEnumerator();
+        }
     }
 }
