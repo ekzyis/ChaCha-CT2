05d9d92aa51dc7674e76f85b905d6e8bced4bbfe
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-08 08:10:53 +0100

    BooleanOr is needed, not BooleanOrVisibility

--- ChaChaVisualizationV2/Helper/Converter/BooleanOr.cs
+++ ChaChaVisualizationV2/Helper/Converter/BooleanOr.cs
@@ -6,12 +6,12 @@ using System.Windows.Data;
 
 namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
 {
-    internal class BooleanOrVisibility : IMultiValueConverter
+    internal class BooleanOr : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values.Any(v => v == DependencyProperty.UnsetValue)) return DependencyProperty.UnsetValue;
-            return values.Any(v => (bool)v == true) ? Visibility.Visible : Visibility.Collapsed;
+            return values.Any(v => (bool)v == true);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
