2aac9f256459f302aa955dd303f039ea5bf6b96a
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-09 12:15:22 +0100

    Define XAML markup for round navigation

---
+++ ChaChaVisualizationV2/Helper/Converter/IsEven.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cryptool.Plugins.ChaChaVisualizationV2.Helper.Converter
+{
+    internal class IsEven : IValueConverter
+    {
+        public bool Inverted { get; set; } = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isEven = (int)value % 2 == 0;
+            return Inverted ? !isEven : isEven;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
