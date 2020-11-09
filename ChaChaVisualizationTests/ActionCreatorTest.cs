ae8cc1bf17a67cc8fc52044d8c2757c2e203eff0
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-09 21:44:37 +0100

    Add unit test project

---
+++ ChaChaVisualizationTests/ActionCreatorTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChaChaVisualizationTests
+{
+    [TestClass]
+    public class ActionCreatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
