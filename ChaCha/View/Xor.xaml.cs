cd3bf4e2c43b4c6eb532c1ae2e0da01ab0ead576
ekzyis <r.gill@stud.uni-heidelberg.de>
2021-02-19 17:41:54 +0100

    Basic XOR view

--- ChaCha/View/Xor.xaml.cs
+++ ChaCha/View/Xor.xaml.cs
@@ -1,28 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cryptool.Plugins.ChaCha.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Cryptool.Plugins.ChaCha.View
 {
     /// <summary>
     /// Interaction logic for Xor.xaml
     /// </summary>
+    [PluginBase.Attributes.Localization("Cryptool.Plugins.ChaCha.Properties.Resources")]
     public partial class Xor : UserControl
     {
         public Xor()
         {
             InitializeComponent();
+            ActionViewBase.LoadLocaleResources(this);
+            this.DataContextChanged += OnDataContextChanged;
+        }
+
+        private XorViewModel ViewModel { get; set; }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            XorViewModel ViewModel = (XorViewModel)e.NewValue;
+            if (ViewModel != null)
+            {
+                this.ViewModel = ViewModel;
+                if (ViewModel.DiffusionActive) InitDiffusionValues();
+            }
+        }
+
+        /// <summary>
+        /// Init the diffusion values for the keystream and the output text.
+        /// </summary>
+        private void InitDiffusionValues()
+        {
+            Plugins.ChaCha.ViewModel.Components.Diffusion.InitDiffusionValue(DiffusionKeystream, ViewModel.ChaCha.KeystreamDiffusion.ToArray(), ViewModel.ChaCha.Keystream.ToArray());
+            Plugins.ChaCha.ViewModel.Components.Diffusion.InitXORValue(DiffusionKeystreamXOR, ViewModel.ChaCha.KeystreamDiffusion.ToArray(), ViewModel.ChaCha.Keystream.ToArray());
+            Plugins.ChaCha.ViewModel.Components.Diffusion.InitDiffusionValue(DiffusionOutput, ViewModel.ChaCha.OutputDiffusion.ToArray(), ViewModel.ChaCha.Output.ToArray());
+            Plugins.ChaCha.ViewModel.Components.Diffusion.InitXORValue(DiffusionOutputXOR, ViewModel.ChaCha.OutputDiffusion.ToArray(), ViewModel.ChaCha.Output.ToArray());
         }
     }
 }
